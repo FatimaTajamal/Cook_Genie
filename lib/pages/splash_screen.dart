3a771d6c73257563f6283bef2f880f3030f59f02
FatimaTajamal <fatimatajamal1@gmail.com>
2025-08-21 21:56:47 +0500

    Building apk

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      if (mounted) {
+        Get.offNamed('/login'); // Redirect to Login
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Full-screen background image
+          Image.asset('lib/images/genie.png', fit: BoxFit.cover),
+          // Optional: You can overlay a semi-transparent color or loading spinner
+          Center(child: CircularProgressIndicator(color: Colors.white)),
+        ],
+      ),
+    );
+  }
+}
